68f6019b735c4f9ee8900e8bf2b131acfbfb6d10
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2019-07-15 09:43:06 +0200

    tests: pin pytest <5.0
    * Pins pytest <5.0.0 due to incompatibility with Python 2.7.

--- setup.py
+++ setup.py
@@ -17,12 +17,12 @@ history = open('CHANGES.rst').read()
 
 tests_require = [
     'check-manifest>=0.25',
-    'coverage>=4.0',
+    'coverage>=4.5.3',
     'isort>=4.2.2',
-    'pydocstyle>=2.0.0',
-    'pytest-cov>=1.8.0',
+    'pydocstyle>=3.0.0',
+    'pytest-cov>=2.7.1',
     'pytest-pep8>=1.0.6',
-    'pytest>=2.8.0',
+    'pytest>=4.6.4,<5.0.0',
     'redis>=2.10.5',
 ]
 
