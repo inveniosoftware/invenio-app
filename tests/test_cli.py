0f250a7a96d03ccd7d10e44bb79e33d8cd966538
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-05-24 20:06:49 +0200

    fix: click breaking changes

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,11 +2,14 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2017-2018 CERN.
+# Copyright (C) 2025 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
 """CLI test."""
+
+import importlib.metadata
 
 from click.testing import CliRunner
 
@@ -16,4 +19,9 @@ def test_basic_cli():
     from invenio_app.cli import cli
 
     res = CliRunner().invoke(cli)
-    assert res.exit_code == 0
+
+    if importlib.metadata.version("click") < "8.2.0":
+        # click >= 8.2.0 dropped python3.9 support
+        assert res.exit_code == 0
+    else:
+        assert res.exit_code == 2
