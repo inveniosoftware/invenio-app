779e017950f6cb6e6638cef38a87e5fafd924d5b
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-07-02 21:35:38 +0200

    release: v2.3.0

--- invenio_app/__init__.py
+++ invenio_app/__init__.py
@@ -2,7 +2,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2017-2018 CERN.
-# Copyright (C) 2024 Graz University of Technology.
+# Copyright (C) 2024-2025 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -170,7 +170,7 @@ to overwrite any existing Invenio files.
 
 from .ext import InvenioApp
 
-__version__ = "2.2.0"
+__version__ = "2.3.0"
 
 __all__ = (
     "__version__",
