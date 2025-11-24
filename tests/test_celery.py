1f77ca3e4e21b482de60e36b5064c73a20e71fdf
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-11-24 23:28:31 +0100

    fix: turn off warning SECRET_KEY
    * Set configuration variable SECRET_KEY with random string

--- tests/test_celery.py
+++ tests/test_celery.py
@@ -2,15 +2,19 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2017-2018 CERN.
+# Copyright (C) 2025 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
 """Celery test."""
 
+import os
+
 
 def test_celery():
     """Test celery application."""
+    os.environ["INVENIO_SECRET_KEY"] = "CHANGE_ME"
     from invenio_app.celery import celery
 
     celery.loader.import_default_modules()
