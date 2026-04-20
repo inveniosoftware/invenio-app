4d2879fa76daa857da41ba05062c9ff8c3408258
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-04-20 12:11:51 +0200

    feat(ext): provide easier-to-access Flask-Talisman instance
    * addresses https://github.com/inveniosoftware/invenio-app/issues/22

--- invenio_app/__init__.py
+++ invenio_app/__init__.py
@@ -168,11 +168,12 @@ better to provide them via an installabled Python package. Do take care not
 to overwrite any existing Invenio files.
 """
 
-from .ext import InvenioApp
+from .ext import InvenioApp, talisman
 
 __version__ = "3.0.0"
 
 __all__ = (
     "__version__",
     "InvenioApp",
+    "talisman",
 )
