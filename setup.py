0fd2ee1bce9a9798363e4f4d7cdf91103dbcf0d7
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-06-29 23:35:47 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools
    * translate the i18n/translation mechanisms as well
    * bump requires-python due to dependencies requiring 3.9+
    * declare conflicting extras for uv
    * format pyproject.toml with Tombi
    
    Co-authored-by: Sam Arbid <samaa2@kth.se>

--- setup.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2017-2018 CERN.
-# SPDX-FileCopyrightText: 2021 TU Wien.
-# SPDX-License-Identifier: MIT
-
-"""WSGI, Celery and CLI applications for Invenio flavours."""
-
-from setuptools import setup
-
-setup()
